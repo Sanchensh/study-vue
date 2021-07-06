f9559288ed301da4d5904f16007aadfbecddebdd
Xu <jlxu@sanchen.local>
2021-07-06 17:42:12 +0800

    delete update

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -1,31 +1,97 @@
 <template>
   <div>
-    <el-row>
-      <el-col :span="24"><div class="grid-content bg-purple-dark"></div></el-col>
-    </el-row>
-    <el-row>
-      <el-col :span="12"><div class="grid-content bg-purple"></div></el-col>
-      <el-col :span="12"><div class="grid-content bg-purple-light"></div></el-col>
-    </el-row>
-    <el-row>
-      <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
-      <el-col :span="8"><div class="grid-content bg-purple-light"></div></el-col>
-      <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
-    </el-row>
-    <el-row>
-      <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
-      <el-col :span="6"><div class="grid-content bg-purple-light"></div></el-col>
-      <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
-      <el-col :span="6"><div class="grid-content bg-purple-light"></div></el-col>
-    </el-row>
-    <el-row>
-      <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
-      <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
-      <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
-      <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
-      <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
-      <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
-    </el-row>
+    <el-container style="height: 500px; border: 1px solid #eee">
+      <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
+        <el-menu :default-openeds="['1', '3']">
+          <el-submenu index="1">
+            <template slot="title"><i class="el-icon-message"></i>导航一</template>
+            <el-menu-item-group>
+              <template slot="title">分组一</template>
+              <el-menu-item index="1-1">选项1</el-menu-item>
+              <el-menu-item index="1-2">选项2</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="分组2">
+              <el-menu-item index="1-3">选项3</el-menu-item>
+            </el-menu-item-group>
+            <el-submenu index="1-4">
+              <template slot="title">选项4</template>
+              <el-menu-item index="1-4-1">选项4-1</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+          <el-submenu index="2">
+            <template slot="title"><i class="el-icon-menu"></i>导航二</template>
+            <el-menu-item-group>
+              <template slot="title">分组一</template>
+              <el-menu-item index="2-1">选项1</el-menu-item>
+              <el-menu-item index="2-2">选项2</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="分组2">
+              <el-menu-item index="2-3">选项3</el-menu-item>
+            </el-menu-item-group>
+            <el-submenu index="2-4">
+              <template slot="title">选项4</template>
+              <el-menu-item index="2-4-1">选项4-1</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+          <el-submenu index="3">
+            <template slot="title"><i class="el-icon-setting"></i>导航三</template>
+            <el-menu-item-group>
+              <template slot="title">分组一</template>
+              <el-menu-item index="3-1">选项1</el-menu-item>
+              <el-menu-item index="3-2">选项2</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="分组2">
+              <el-menu-item index="3-3">选项3</el-menu-item>
+            </el-menu-item-group>
+            <el-submenu index="3-4">
+              <template slot="title">选项4</template>
+              <el-menu-item index="3-4-1">选项4-1</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+
+      <el-container>
+        <el-header style="text-align: right; font-size: 12px">
+          <el-dropdown>
+            <i class="el-icon-setting" style="margin-right: 15px"></i>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>查看</el-dropdown-item>
+              <el-dropdown-item>新增</el-dropdown-item>
+              <el-dropdown-item>删除</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+          <span>王小虎</span>
+        </el-header>
+
+        <el-main>
+          <el-table :data="tableData">
+            <el-table-column label="编号" width="140">
+              <template slot-scope="scope">
+                <!--                <i class="el-icon-time"></i>-->
+                <span style="margin-left: 10px">{{ scope.row.id }}</span>
+              </template>
+            </el-table-column>
+            <el-table-column label="姓名" width="120">
+              <template slot-scope="scope">
+                <span style="margin-left: 10px">{{ scope.row.name }}</span>
+              </template>
+            </el-table-column>
+            <el-table-column label="年龄">
+              <template slot-scope="scope">
+                <span style="margin-left: 10px">{{ scope.row.age }}</span>
+              </template>
+            </el-table-column>
+            <el-table-column>
+              <template slot-scope="scope">
+                <button class="update_button" v-on:click="updateUser(scope.row)">更新</button>
+                <button class="delete_button" v-on:click="deleteUser(scope.row.id)">删除</button>
+              </template>
+            </el-table-column>
+          </el-table>
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
@@ -33,59 +99,53 @@
 export default {
   data() {
     return {
-      message: '页面加载于' + new Date().toLocaleDateString(),
-      user: 'I am xu jianglei',
-      seen: false,
-      todos: [{text: '学习 JavaScript'},
-        {text: '学习 Vue'},
-        {text: '整个牛项目'}],
+      tableData: []
     }
-  }, methods: {
-    my_first_click: function () {
-      this.user = this.user.split('').reverse().join('')
-    },
-    loadData() {
-      console.log('called')
+  },
+  methods: {
+    findAll() {
       let _this = this;
       this.$ajax.get('http://localhost:8081/user').then(
           function (res) {
-            _this.user = res.data.data
+            _this.tableData = res.data.data
           }
       ).catch(function (res) {
         console.log(res)
+      })
+    },
+    updateUser(row) {
+      row.name = 'hhh'
+      this.$ajax.put('http://localhost:8081/user', row).then(function (res) {
+        let result = res.data.data
+        if (result === 1) {
+          console.log("success")
+        }
+      })
+    },
+    deleteUser(id) {
+      this.$ajax.delete('http://localhost:8081/user/' + id).then(function (res) {
+        let result = res.data.data
+        if (result === 1) {
+          console.log("success")
+        }
       })
     }
   },
   created: function () {
     console.log("init......")
-    this.loadData();
+    this.findAll();
   }
 }
 </script>
 
 <style scoped>
-.el-row {
-  margin-bottom: 20px;
-}
-.el-col {
-  border-radius: 4px;
-}
-.bg-purple-dark {
-  background: #99a9bf;
-}
-.bg-purple {
-  background: #d3dce6;
-}
-.bg-purple-light {
-  background: #e5e9f2;
-}
-.grid-content {
-  border-radius: 4px;
-  min-height: 36px;
-}
-.row-bg {
-  padding: 10px 0;
-  background-color: #f9fafc;
+.update_button {
+  margin-left: 10px;
+  background-color: yellow;
 }
 
+.delete_button {
+  margin-left: 10px;
+  background-color: red;
+}
 </style>
